d773a9cfe7da5b29c9d9128493e4fe7328586e18
yoha52 <yohachandera@gmail.com>
2026-07-03 14:29:45 +0300

    add trust proxy and fix ssl mode

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-
+app.set('trust proxy', 1);
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
